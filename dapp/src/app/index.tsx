dd3a4a1a84de96901a3b1ada3d14fdd804f22903
Jingyu <jingyu.zhang@appfolio.com>
2018-06-13 22:42:38 -0700

    add reactstrap, font awesome packages and bootstrap socail styles

--- dapp/src/app/index.tsx
+++ dapp/src/app/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router';
 import OpenColony from 'app/containers/OpenColony';
 import { hot } from 'react-hot-loader';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export const App = hot(module)(() => (
   <Switch>
