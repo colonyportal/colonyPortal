d4e71211a7554da77c8c81deecd953240c6dcee3
Jingyu Zhang <jingyu.zhang@appfolio.com>
2018-06-15 15:44:47 -0700

    jingyu - remove Todo stuff these come with boiler plate, keep code cleaner

--- dapp/src/app/actions/index.ts
+++ dapp/src/app/actions/index.ts
@@ -1,1 +1,4 @@
-export * from './todos';
+// TODO: I think it's a good pattern to put everything in index
+// but right now we are just importing from every single module
+// let's figure it out later
+export * from './tasks';
