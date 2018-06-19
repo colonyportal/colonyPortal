8fa36bebc05cdda6f0b486ae82ded190bc7400f5
Asgeir Sognefest <sogasg@gmail.com>
2018-06-19 23:44:52 +0200

    Update the NAV with the new routes

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -19,13 +19,16 @@ const nav: SFC<Props> = ({colonyAddress}) => (
     <Collapse isOpen>
       <Nav className="ml-auto d-flex flex-row" navbar>
         <NavItem className="ml-3">
-          <NavLink href={`/${colonyAddress}/dashboard`}>Dashboard</NavLink>
+          <NavLink href={`/${colonyAddress}`}>Dashboard</NavLink>
         </NavItem>
          <NavItem className="ml-3">
           <NavLink href={`/${colonyAddress}/tasks`}>Tasks</NavLink>
         </NavItem>
+         <NavItem className="ml-3">
+          <NavLink href={`/${colonyAddress}/create_task`}>Create Task</NavLink>
+        </NavItem>
         <NavItem className="ml-3">
-          <NavLink href="/my_tasks">My Tasks</NavLink>
+          <NavLink href={`/${colonyAddress}/my_tasks`}>My Tasks</NavLink>
         </NavItem>
       </Nav>
     </Collapse>
