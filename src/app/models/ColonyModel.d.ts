70453c2803d907eec691dcee81736a8b8fb3a243
jordanellis6809 <jordanellis6809@gmail.com>
2018-06-21 16:49:50 -0700

    Removed uneeded files, removed indents

--- src/app/models/ColonyModel.d.ts
+++ src/app/models/ColonyModel.d.ts
@@ -2,48 +2,47 @@
 // reference: https://docs.colony.io/colonyjs/api-colonyclient/#gettaskcall-taskid-
 
 export type Task = {
-    /** Boolean flag denoting whether the task is cancelled.*/
-    cancelled: boolean;
-  
-    /** Date when the deliverable is due.*/
-    deliverableDate?: string;
-  
-    /** IPFS hash unique hash of the deliverable content.*/
-    deliverableHash?: string;
-  
-    /**	Integer Domain ID the task belongs to. */
-    domainId: number;
-  
-    /** Date when the task is due. */
-    dueDate?: string;
-  
-    /** Boolean flag denoting whether the task is finalized. */
-    finalized: boolean;
-  
-    /** Integer task ID. */
-    id: number;
-  
-    /** Number of payouts that cannot be completed with the current task funding. */
-    payoutsWeCannotMake?: number;
-  
-    /** Integer ID of funding pot for the task. */
-    potId?: number;
-  
-    /** Integer Skill ID the task is assigned to. */
-    skillId: number;
-  
-    /** IPFS hash	Unique hash of the specification content. */
-    specificationHash: string;
-  };
-  
-  export type Domain = {
-    /** ID of the domain. */
-    domainId: number;
-  
-    /** The domain's local skill ID. */
-    localSkillId: number;
-  
-    /** The domain's funding pot ID. */
-    potId: number;
-  };
-  
+  /** Boolean flag denoting whether the task is cancelled.*/
+  cancelled: boolean;
+
+  /** Date when the deliverable is due.*/
+  deliverableDate?: string;
+
+  /** IPFS hash unique hash of the deliverable content.*/
+  deliverableHash?: string;
+
+  /**	Integer Domain ID the task belongs to. */
+  domainId: number;
+
+  /** Date when the task is due. */
+  dueDate?: string;
+
+  /** Boolean flag denoting whether the task is finalized. */
+  finalized: boolean;
+
+  /** Integer task ID. */
+  id: number;
+
+  /** Number of payouts that cannot be completed with the current task funding. */
+  payoutsWeCannotMake?: number;
+
+  /** Integer ID of funding pot for the task. */
+  potId?: number;
+
+  /** Integer Skill ID the task is assigned to. */
+  skillId: number;
+
+  /** IPFS hash	Unique hash of the specification content. */
+  specificationHash: string;
+};
+
+export type Domain = {
+  /** ID of the domain. */
+  domainId: number;
+
+  /** The domain's local skill ID. */
+  localSkillId: number;
+
+  /** The domain's funding pot ID. */
+  potId: number;
+};
