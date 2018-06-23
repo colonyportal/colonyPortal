cf1ae158dea984695f11376057b9a171a1d2dcbb
Jingyu <jingyu.zhang@appfolio.com>
2018-06-23 00:38:46 -0700

    add taskDetail type for task related attributes that are not saved in task specifications

---
+++ src/app/models/taskDetail.d.ts
@@ -0,0 +1,8 @@
+/** Task details that are not included in task spec contents **/
+
+export type TaskDetail = {
+  taskId: number;
+  managerPayout: number,
+  workerPayout: number,
+  evaluatorPayout: number,
+};
